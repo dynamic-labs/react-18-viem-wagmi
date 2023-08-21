117b6dc965e5e8a980323c9828e9be593ff9c0d2
Matthew Foyle <mfoyle93@gmail.com>
2023-08-21 13:41:08 +0200

    feat: add wagmi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { DynamicContextProvider, DynamicWidget } from "@dynamic-labs/sdk-react";
 
-const App = () => (
-  <DynamicContextProvider
+import { DynamicWagmiConnector } from '@dynamic-labs/wagmi-connector@viem';
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react';
+const App = () => {
+return (
+    <DynamicContextProvider
     settings={{
       environmentId: "8b5038f9-171c-480b-9f3c-294d868aeb74",
     }}
-  >
-    <DynamicWidget />
-  </DynamicContextProvider>
+    >
+    <DynamicWagmiConnector>
+        <DynamicWidget />
+    </DynamicWagmiConnector>
+    </DynamicContextProvider>
 );
+}
 
 export default App;
