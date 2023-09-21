6462db76afd3b6526b81c156cfa8690b95f1ceb0
Matthew Foyle <mfoyle93@gmail.com>
2023-09-21 15:00:27 +0200

    chore: remove unwanted libs and code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
-import {
-  DynamicContextProvider,
-  DynamicWidget,
-} from "@dynamic-labs/sdk-react";
-
-import Home from "./components/Home";
-
+import { DynamicContextProvider, DynamicWidget } from "@dynamic-labs/sdk-react";
 
 const App = () => {
   return (
     <DynamicContextProvider
       settings={{
-        environmentId: "4e598b41-f388-489b-a0b3-d24064b1d1ed"
+        environmentId: "4e598b41-f388-489b-a0b3-d24064b1d1ed",
       }}
     >
       <DynamicWagmiConnector>
         <DynamicWidget />
-        <Home />
       </DynamicWagmiConnector>
     </DynamicContextProvider>
   );
 };
 
 export default App;
-
-
