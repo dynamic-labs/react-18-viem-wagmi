537ca1c86d63ddd24c9bd43d2a80a894337896eb
Yoni Goldberg <yoni@dynamic.xyz>
2023-11-09 23:57:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import {
   DynamicContextProvider,
   DynamicWidget,
@@ -27,11 +26,28 @@ const App = () => {
           MagicWalletConnectors,
           BloctoEvmWalletConnectors,
         ],
+        evmNetworks: [
+          {
+            blockExplorerUrls: ["https://cronoscan.com/"],
+            chainId: 25,
+            chainName: "Cronos Mainnet",
+            iconUrls: [
+              "https://www.gitbook.com/cdn-cgi/image/width=40,dpr=2,height=40,fit=contain,format=auto/https%3A%2F%2F4017632223-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FqXsIo4b4WtOTfICyOIxa%252Ficon%252FjttFGLyNrcsRyiavSGOl%252FCGzQIEVX_400x400.png%3Falt%3Dmedia%26token%3D9955a6fa-7d27-48a0-bc9d-534e107668d8",
+            ],
+            name: "Cronos",
+            nativeCurrency: {
+              decimals: 18,
+              name: "Cronos",
+              symbol: "CRO",
+            },
+            networkId: 25,
+            rpcUrls: ["https://evm.cronos.org"],
+            vanityName: "Cronos",
+          },
+        ],
       }}
     >
-      <DynamicWagmiConnector>
-        <DynamicWidget />
-      </DynamicWagmiConnector>
+        <DynamicWidget />      
     </DynamicContextProvider>
   );
 };
